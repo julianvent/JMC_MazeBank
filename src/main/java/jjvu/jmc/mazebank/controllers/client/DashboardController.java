cf8b3beff2901b4f2d24b41d36e276bfd16e0c5a
Julian <zS22017046@estudiantes.uv.mx>
2024-10-01 12:03:10 -0600

    dashboard controller updated

--- src/main/java/jjvu/jmc/mazebank/controllers/client/DashboardController.java
+++ src/main/java/jjvu/jmc/mazebank/controllers/client/DashboardController.java
@@ -1,4 +1,30 @@
 package jjvu.jmc.mazebank.controllers.client;
 
-public class DashboardController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    public Text userName;
+    public Label loginDate;
+    public Label checkingBalance;
+    public Label checkingAccountNumber;
+    public Label savingsBalance;
+    public Label savingsAccountNumber;
+    public Text incomeLabel;
+    public Label expenseLabel;
+    public ListView transactionListView;
+    public TextField payeeField;
+    public TextField amountField;
+    public TextArea messageField;
+    public Button sendButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
