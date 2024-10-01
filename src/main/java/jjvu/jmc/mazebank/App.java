8a3205bdf617ac60c9e7a938543eb52d08a505e8
Julian <zS22017046@estudiantes.uv.mx>
2024-10-01 12:40:04 -0600

    ViewFactory setup [showLogin, showClient]

--- src/main/java/jjvu/jmc/mazebank/App.java
+++ src/main/java/jjvu/jmc/mazebank/App.java
@@ -4,16 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import jjvu.jmc.mazebank.views.ViewFactory;
 
 import java.io.IOException;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        ViewFactory viewFactory = new ViewFactory();
 
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
+        viewFactory.showLoginWindow();
     }
 }
--- src/main/java/jjvu/jmc/mazebank/views/ViewFactory.java
+++ src/main/java/jjvu/jmc/mazebank/views/ViewFactory.java
@@ -1,4 +1,53 @@
 package jjvu.jmc.mazebank.views;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import jjvu.jmc.mazebank.controllers.client.ClientController;
+
 public class ViewFactory {
+    // Client Views
+    private AnchorPane dashboardView;
+
+    public ViewFactory() {}
+
+    public AnchorPane getDashboardView() {
+        if (dashboardView == null) {
+            try {
+                dashboardView = new FXMLLoader(getClass().getResource("/fxml/client/Dashboard.fxml")).load();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return dashboardView;
+    }
+
+    private void createStage(FXMLLoader loader) {
+        Scene scene = null;
+
+        try {
+            scene = new Scene(loader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Maze Bank");
+        stage.show();
+    }
+
+    public void showLoginWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        createStage(loader);
+    }
+
+    public void showClientWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/client/Client.fxml"));
+        ClientController clientController = new ClientController();
+        loader.setController(clientController);
+
+        createStage(loader);
+    }
 }
