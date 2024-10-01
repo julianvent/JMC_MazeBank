1ec95cd08a36c6019c9776ba0f75754a3f10dc28
Julian <zS22017046@estudiantes.uv.mx>
2024-10-01 00:00:18 -0600

    client menu part 2

--- src/main/java/jjvu/jmc/mazebank/controllers/client/ClientMenuController.java
+++ src/main/java/jjvu/jmc/mazebank/controllers/client/ClientMenuController.java
@@ -1,4 +1,21 @@
 package jjvu.jmc.mazebank.controllers.client;
 
-public class ClientMenuController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboardButton;
+    public Button transactionButton;
+    public Button accountButton;
+    public Button profileButton;
+    public Button logoutButton;
+    public Button reportButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
