a6bff4feb33ee41bad75a8529d7aafaa469135de
Julian <zS22017046@estudiantes.uv.mx>
2024-10-01 15:49:54 -0600

    AdminController setup

--- src/main/java/jjvu/jmc/mazebank/controllers/admin/AdminMenuController.java
+++ src/main/java/jjvu/jmc/mazebank/controllers/admin/AdminMenuController.java
@@ -1,4 +1,19 @@
 package jjvu.jmc.mazebank.controllers.admin;
 
-public class AdminMenuController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+    public Button createClientButton;
+    public Button listClientsButton;
+    public Button depositButton;
+    public Button logoutButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
