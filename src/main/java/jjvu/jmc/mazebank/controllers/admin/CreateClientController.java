d5fae3aaaeca22235560ddbdb0b54e41128adb03
Julian <zS22017046@estudiantes.uv.mx>
2024-10-01 16:17:16 -0600

    CreateClient controller updated

--- src/main/java/jjvu/jmc/mazebank/controllers/admin/CreateClientController.java
+++ src/main/java/jjvu/jmc/mazebank/controllers/admin/CreateClientController.java
@@ -1,4 +1,29 @@
 package jjvu.jmc.mazebank.controllers.admin;
 
-public class CreateClientController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateClientController implements Initializable {
+    public TextField firstNameField;
+    public TextField lastNameField;
+    public TextField passwordField;
+    public CheckBox payeeAddressBox;
+    public Label payeeAddressLabel;
+    public CheckBox addCheckingAccountBox;
+    public TextField checkingAmountField;
+    public CheckBox addSavingsAccountBox;
+    public TextField savingsAmountField;
+    public Button createClientButton;
+    public Label errorLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
