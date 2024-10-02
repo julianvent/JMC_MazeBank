8913698002c4490ad25ee072a64dc635ce9a9b88
Julian <zS22017046@estudiantes.uv.mx>
2024-10-02 09:42:21 -0600

    admin DepositController updated to fxml

--- src/main/java/jjvu/jmc/mazebank/controllers/admin/DepositController.java
+++ src/main/java/jjvu/jmc/mazebank/controllers/admin/DepositController.java
@@ -1,11 +1,20 @@
 package jjvu.jmc.mazebank.controllers.admin;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DepositController implements Initializable {
+    public TextField payeeAddressSearchField;
+    public Button searchButton;
+    public ListView resultsListView;
+    public TextField depositAmountField;
+    public Button depositButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
