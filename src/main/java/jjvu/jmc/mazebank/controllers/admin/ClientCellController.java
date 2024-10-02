81fda569c9013c2a63537ac89acb7253ca47b16a
Julian <zS22017046@estudiantes.uv.mx>
2024-10-02 13:46:09 -0600

    admin ClientCell css applied and controller updated to fxml

--- src/main/java/jjvu/jmc/mazebank/controllers/admin/ClientCellController.java
+++ src/main/java/jjvu/jmc/mazebank/controllers/admin/ClientCellController.java
@@ -1,11 +1,21 @@
 package jjvu.jmc.mazebank.controllers.admin;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ClientCellController implements Initializable {
+    public Label firstNameLabel;
+    public Label lastNameLabel;
+    public Label payeeAddressLabel;
+    public Label checkingAccountLabel;
+    public Label savingsAccountLabel;
+    public Label dateCreatedLabel;
+    public Button deleteButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
